18acf3bb8c68a51c076d9b18b2dbc57a099a105d
alexsnz23 <30882209+alexsnz23@users.noreply.github.com>
2017-10-12 08:16:22 -0500

    Add files via upload

---
+++ L0gins/src/java/Controlador/Login.java
@@ -0,0 +1,105 @@
+﻿/*
+ * Alumno: Gomez Tapia Hector
+ */
+package Controlador;
+
+import Modelo.BaseDatos;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Login extends HttpServlet {
+    BaseDatos BD = new BaseDatos();
+                                  //Declaracion de objetos de respuesta y peticion
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)//Metodo que se llama en el formulario
+            throws ServletException, IOException, ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) { //Metodo que dibuja todo el contenido HTML
+            /* TODO output your page here. You may use following sample code. */
+            String nombre = request.getParameter("NombreUsuario"); //Nombre
+            //Variables que son recibidas en el formulario
+            String pass = request.getParameter("PassUsuario");//Contraseña
+            Boolean verificacion; //Booleano para indicar estado
+            
+            BD.Conectar(); //LLamar a la conexion 
+            verificacion = BD.BuscarUsuario(nombre, pass);  //Probar la verificacion
+            
+            if(verificacion)   //Verificar la informacion
+            {
+                out.println("Bienvenido " + nombre);  // Impresion del nombre verificado
+            }
+            else
+            {
+                out.println("Acceso denegado"); //Impresion de no haber encontrado el nombre y la contraseña
+                
+            }
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ L0gins/src/java/Modelo/BaseDatos.java
@@ -0,0 +1,45 @@
+﻿package Modelo;
+
+/*
+ * Alumno:Gomez Tapia Hector
+ */
+import java.sql.*;
+import java.io.*;
+
+
+public class BaseDatos { 
+    Connection con = null; //Variables para la base de datos
+    Statement stm = null;
+    ResultSet r = null;
+    
+    public String Conectar() throws ClassNotFoundException, InstantiationException, IllegalAccessException
+    {
+        String mensaje; 
+        try
+            {  //Conexion a la base de datos
+                Class.forName("com.mysql.jdbc.Driver").newInstance();
+                con = DriverManager.getConnection("jdbc:mysql://localhost/LoginPractica", "root", "n0m3l0");
+                stm = con.createStatement(); //Declaracion y asignacion de valores para las variables
+                mensaje = "Conexión exitosa";
+            }catch (SQLException error){
+                mensaje = error.toString();
+            }
+        return mensaje;
+    }
+    
+    public Boolean BuscarUsuario(String Nombre, String Pass ) throws SQLException  //Método que se utiliza para validar
+    {
+        Boolean VerExist = false;
+        r = stm.executeQuery("SELECT * FROM Usuarios;"); //Instruccion SQL
+        
+        while(r.next())
+        {
+            if(Nombre.equals(r.getString("NombreUsuario")) && Pass.equals(r.getString("PassUsuario")))  //Busca meiante el Nombre y Contraseña si existe el campo
+            {
+                VerExist = true;
+            }
+        }
+        
+        return VerExist;  //Regresa un valor a la clase en la que sea llamado
+    }
+}
